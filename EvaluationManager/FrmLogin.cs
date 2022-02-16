2f3585d13f646c7ca50dbc77b314e85b8a6e8370
PIUserTest <kopingagin@sewce.com>
2022-02-16 11:34:53 +0100

    Provjera prijave Implementirana provjera prijave korisnika

---
+++ EvaluationManager/FrmLogin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationManager
+{
+    public partial class FrmLogin : Form
+    {
+        private string username = "prof";
+        private string password = "test";
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtUsername.Text == "" || txtPassword.Text == "")
+            {
+                MessageBox.Show("Unesite korisničke podatke!", "Upozorenje!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(txtUsername.Text == username && txtPassword.Text == password)
+            {
+                MessageBox.Show($"Ulogirani ste kao {txtUsername.Text}");   
+            }
+            else
+            {
+                MessageBox.Show("Neispravni korisnički podaci!");
+            }
+        }
+    }
+}
