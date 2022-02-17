2f5fe9e01f7074b15cd49dc090db4da33ada3ce6
PIUserTest <kopingagin@sewce.com>
2022-02-17 12:31:41 +0100

    Implementacija klase Evaluation

---
+++ EvaluationManager/Models/Evaluation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationManager.Models
+{
+    public class Evaluation
+    {
+        public int Id { get; set; }
+        public Teacher Teacher { get; set; }
+        public DateTime EvaluationDate { get; set; }
+        public int Points { get; set; }
+        public Activity Activity { get; set; }
+
+        public bool IsSufficientForGrade()
+        {
+            return Points >= Activity.MinPointsForGrade;
+        }
+
+        public bool IsSufficientForSignature()
+        {
+            return Points >= Activity.MinPointsForSignature;
+        }
+    }
+}
